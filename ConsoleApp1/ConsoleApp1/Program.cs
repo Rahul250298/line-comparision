9a6f91cc896aa2357ca880cfecbcf26927975b38
Rahul250298 <91786240+Rahul250298@users.noreply.github.com>
2022-01-23 11:17:01 +0530

    m

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("welcome to linecomparision problems");
+            Line_comparision Linecom = new Line_comparision();
+            Linecom.length_line();
+            Linecom.function_check();
+            Console.ReadLine();
         }
     }
 }
